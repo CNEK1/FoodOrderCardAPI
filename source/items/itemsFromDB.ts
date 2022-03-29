65ee4ab5514727d96c2dc585af514d8fc864082b
CNEK1 <darkghostspain1@gmail.com>
2022-03-30 02:09:08 +0300

    Add fetch data from MongoDB

---
+++ source/items/itemsFromDB.ts
@@ -0,0 +1,48 @@
+import { Request,Response,NextFunction } from "express";
+import mongoose from "mongoose";
+import Products from "../models/ProductJSON";
+
+ const getALL = (req:Request,res:Response,next:NextFunction) => {
+    Products.find()
+    .exec()
+    .then(result =>{
+        return res.json({
+            items: result
+        });
+    })
+    .catch((error) => {
+        return res.status(500).json({
+            message:error.message,
+            error
+        })
+    })
+  };
+  const createItem = (req: Request, res:Response, next:NextFunction)=>{
+    let{title,cost,like,amount,order} = req.body;
+
+    const items = new Products({
+        _id: new mongoose.Types.ObjectId(),
+        title,
+        cost,
+        like,
+        amount,
+        order
+    });
+    
+    return items
+    .save()
+    .then((result) => {
+        return res.status(201).json({
+            items: result
+        })
+    })
+    .catch((error) => {
+        return res.status(500).json({
+            message:error.message,
+            error
+        })
+    })
+  };
+
+
+  export default {getALL,createItem};
